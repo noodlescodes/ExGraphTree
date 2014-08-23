478930c2188cd1d84f70a2c5de5efe4f7f793913
Nathan <noodlescodes@gmail.com>
2014-08-23 18:11:46 +1000

    added command line argument to pass in the order to search.

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <ctime>
+#include <sstream>
 #include "BTNode.h"
 #include "Matrix.h"
 #include "Explorer.h"
@@ -9,60 +10,29 @@
 using namespace std;
 using namespace VAN_MAASTRICHT;
 
-bool squares_exist(const Matrix &m) {
-
-	Matrix mm = m * m;
-	Matrix mmmm = mm * mm;
-	int sum = mmmm.trace();
-
-	for(unsigned int i = 0; i < m.size(); i++) {
-		for(unsigned int j = 0; j < m.size(); j++) {
-			sum -= mm.get_entry(i, j);
-		}
+int main(int argc, char* argv[]) {
+	if(argc < 2) {
+		cout << "Need an argument for the order of the graph" << endl;
+		return 1;
+	}
+	if(argc > 2) {
+		cout << "Only provide one argument" << endl;
 	}
 
-	for(unsigned int i = 0; i < m.size(); i++) {
-		int s = 0;
-		for(unsigned int j = 0; j < m.size(); j++) {
-			s += m.get_entry(i, j);
-		}
-		if(s > 1) {
-			sum -= (s * (s - 1));
-		}
-	}
+	stringstream str;
+	str << argv[1];
+	int order;
+	str >> order;
 
-	cout << sum << endl;
-
-	if(sum == 0) {
-		return false;
-	}
-
-	return true;
-}
-
-int main() {
 	time_t start = time(NULL);
 	Matrix b = Matrix();
-	b.set_size(11);
+	b.set_size(order);
 	Explorer *a = new Explorer(new BTNode<Matrix>(b));
 
 	a->explore(a->get_root(), 0, 1, 0);
 	cout << "Number of solutions: " << a->get_number_solutions() << endl;
 	delete a;
 	cout << "Time taken: " << time(NULL) - start << endl;
-
-	/*Matrix b = Matrix();
-	b.set_size(5);
-	b.set_entry(0, 1, 1);
-	b.set_entry(0, 2, 1);
-	b.set_entry(0, 3, 1);
-	b.set_entry(0, 4, 1);
-	b.set_entry(1, 0, 1);
-	b.set_entry(2, 0, 1);
-	b.set_entry(3, 0, 1);
-	b.set_entry(4, 0, 1);
-
-	cout << squares_exist(b) << endl;*/
 
 	return 0;
 }
